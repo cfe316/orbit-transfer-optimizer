c56cd252b61bbefd0a42885908912ad54dc60f94
Jacob Schwartz <thesquarerootofjacob@gmail.com>
2015-02-06 12:00:45 -0500

    Cleaned up and wrote documentation for AcceptableInterval.m

--- AcceptableInterval.m
+++ AcceptableInterval.m
@@ -1,10 +1,46 @@
 (* ::Package:: *)
 
+(* ::Section:: *)
+(* Acceptable Interval package: Title and comments *)
+
+(* :Title: AcceptableInterval *)
+(* :Context: OrbitTransferOptimizer`AcceptableInterval` *)
+(* :Author: Jacob Schwartz (thesquarerootofjacob@gmail.com) *)
+
+(* ::Section:: *)
+(* Begin package and help *)
+
+BeginPackage["OrbitTransferOptimizer`AcceptableInterval`"];
+
+Unprotect@"`*";
+ClearAll@"`*";
+
+Acceptable\[Omega]Interval::usage = "For two points in the plane r1 != r2 and \[Theta]1 != \[Theta]2 and \[Theta]1 != \[Theta]2 + \[Pi], return an Interval corresponding to acceptable values for an orbit's argument of periapsis \[Omega] for an orbit that passes through both these points.
+
+\[Omega] is acceptable if p is positive (orbit is not on the wrong branch of a hyperbola) and if e>1 (it's a hyperbola) then it has to reach 'point 2' after 'point 1'.
+
+Input format:
+{r1, \[Theta]1}, {r2, \[Theta]2}: the initial and final points that the orbit goes through.
+
+Output format:
+{
+	{1, Interval[{...}]},
+	{-1, Interval[{...}]},
+}
+
+Where the first is the Interval acceptable for if the orbit is counterclockwise (sign of h is +1) and the second is for if the orbit is clockwise (sign of h is -1).
+";
+
+Begin["`Private`"];
+
 (* ::Text:: *)
 (*helper function that takes an interval (which may have multiple ... spans?) and a point, and returns one part of that interval, as an inequality for \[Omega].*)
 
+getInequalityForPoint[a_Interval,p_] := Module[{s},
+	s = Select[ Table[ Interval[ a[[i]]], {i, 1, Length[a]}], IntervalMemberQ[#, p] & ][[1,1]];
+	s[[1]] < \[Omega] < s[[2]]
+]
 
-getInequalityForPoint[a_Interval,p_]:=Module[{s},s=Select[Table[Interval[a[[i]]],{i,1,Length[a]}],IntervalMemberQ[#,p]&][[1,1]];s[[1]]<\[Omega]<s[[2]]]
 
 
 (* ::Section:: *)
@@ -12,176 +48,197 @@ getInequalityForPoint[a_Interval,p_]:=Module[{s},s=Select[Table[Interval[a[[i]]]
 
 
 (* ::Text:: *)
-(*Returns the two angles of \[Omega] for which \[Epsilon] is equal to 1.*)
+(*Returns the two angles of \[Omega] for which e is equal to 1.*)
 
 
-\[Epsilon]Is1l[{r1_,\[Theta]1_},{r2_,\[Theta]2_}]:=\[Epsilon]Is1c[r1,\[Theta]1,r2,\[Theta]2];\[Epsilon]Is1c=Compile[{{r1,_Real},{\[Theta]1,_Real},{r2,_Real},{\[Theta]2,_Real}},Block[{pts},pts={-ArcCos[1/(r1^2+r2^2-2 r1 r2 Cos[\[Theta]1-\[Theta]2]) (-r1^2 Cos[\[Theta]1]+r1 r2 Cos[\[Theta]1]+r1 r2 Cos[\[Theta]2]-r2^2 Cos[\[Theta]2]-2 \[Sqrt](r1^3 r2 Sin[\[Theta]1]^2 Sin[\[Theta]1/2-\[Theta]2/2]^2-2 r1^2 r2^2 Sin[\[Theta]1] Sin[\[Theta]1/2-\[Theta]2/2]^2 Sin[\[Theta]2]+r1 r2^3 Sin[\[Theta]1/2-\[Theta]2/2]^2 Sin[\[Theta]2]^2))],ArcCos[1/(r1^2+r2^2-2 r1 r2 Cos[\[Theta]1-\[Theta]2]) (-r1^2 Cos[\[Theta]1]+r1 r2 Cos[\[Theta]1]+r1 r2 Cos[\[Theta]2]-r2^2 Cos[\[Theta]2]-2 \[Sqrt](r1^3 r2 Sin[\[Theta]1]^2 Sin[\[Theta]1/2-\[Theta]2/2]^2-2 r1^2 r2^2 Sin[\[Theta]1] Sin[\[Theta]1/2-\[Theta]2/2]^2 Sin[\[Theta]2]+r1 r2^3 Sin[\[Theta]1/2-\[Theta]2/2]^2 Sin[\[Theta]2]^2))],-ArcCos[1/(r1^2+r2^2-2 r1 r2 Cos[\[Theta]1-\[Theta]2]) (-r1^2 Cos[\[Theta]1]+r1 r2 Cos[\[Theta]1]+r1 r2 Cos[\[Theta]2]-r2^2 Cos[\[Theta]2]+2 \[Sqrt](r1^3 r2 Sin[\[Theta]1]^2 Sin[\[Theta]1/2-\[Theta]2/2]^2-2 r1^2 r2^2 Sin[\[Theta]1] Sin[\[Theta]1/2-\[Theta]2/2]^2 Sin[\[Theta]2]+r1 r2^3 Sin[\[Theta]1/2-\[Theta]2/2]^2 Sin[\[Theta]2]^2))],ArcCos[1/(r1^2+r2^2-2 r1 r2 Cos[\[Theta]1-\[Theta]2]) (-r1^2 Cos[\[Theta]1]+r1 r2 Cos[\[Theta]1]+r1 r2 Cos[\[Theta]2]-r2^2 Cos[\[Theta]2]+2 \[Sqrt](r1^3 r2 Sin[\[Theta]1]^2 Sin[\[Theta]1/2-\[Theta]2/2]^2-2 r1^2 r2^2 Sin[\[Theta]1] Sin[\[Theta]1/2-\[Theta]2/2]^2 Sin[\[Theta]2]+r1 r2^3 Sin[\[Theta]1/2-\[Theta]2/2]^2 Sin[\[Theta]2]^2))]};Sort[Mod[Select[pts,Abs[-((r1-r2)/(r1 Cos[\[Theta]1-#1]-r2 Cos[\[Theta]2-#1]))-1.]<1/10^10&],2 \[Pi]]]],CompilationTarget->"C",RuntimeOptions->"Speed"];
+eIs1l[{r1_,th1_},{r2_,th2_}] := eIs1c[r1,th1,r2,th2];
+eIs1c = Compile[{{r1,_Real},{th1,_Real},{r2,_Real},{th2,_Real}},
+	Block[{pts},pts = {
+		-ArcCos[1/(r1^2+r2^2-2 r1 r2 Cos[th1-th2]) (-r1^2 Cos[th1]+r1 r2 Cos[th1]+r1 r2 Cos[th2]-r2^2 Cos[th2]-2 \[Sqrt](r1^3 r2 Sin[th1]^2 Sin[th1/2-th2/2]^2-2 r1^2 r2^2 Sin[th1] Sin[th1/2-th2/2]^2 Sin[th2]+r1 r2^3 Sin[th1/2-th2/2]^2 Sin[th2]^2))],
+		ArcCos[1/(r1^2+r2^2-2 r1 r2 Cos[th1-th2]) (-r1^2 Cos[th1]+r1 r2 Cos[th1]+r1 r2 Cos[th2]-r2^2 Cos[th2]-2 \[Sqrt](r1^3 r2 Sin[th1]^2 Sin[th1/2-th2/2]^2-2 r1^2 r2^2 Sin[th1] Sin[th1/2-th2/2]^2 Sin[th2]+r1 r2^3 Sin[th1/2-th2/2]^2 Sin[th2]^2))],
+		-ArcCos[1/(r1^2+r2^2-2 r1 r2 Cos[th1-th2]) (-r1^2 Cos[th1]+r1 r2 Cos[th1]+r1 r2 Cos[th2]-r2^2 Cos[th2]+2 \[Sqrt](r1^3 r2 Sin[th1]^2 Sin[th1/2-th2/2]^2-2 r1^2 r2^2 Sin[th1] Sin[th1/2-th2/2]^2 Sin[th2]+r1 r2^3 Sin[th1/2-th2/2]^2 Sin[th2]^2))],
+		ArcCos[1/(r1^2+r2^2-2 r1 r2 Cos[th1-th2]) (-r1^2 Cos[th1]+r1 r2 Cos[th1]+r1 r2 Cos[th2]-r2^2 Cos[th2]+2 \[Sqrt](r1^3 r2 Sin[th1]^2 Sin[th1/2-th2/2]^2-2 r1^2 r2^2 Sin[th1] Sin[th1/2-th2/2]^2 Sin[th2]+r1 r2^3 Sin[th1/2-th2/2]^2 Sin[th2]^2))]
+	};
+	Sort[ Mod[ Select[ pts, Abs[-((r1-r2)/(r1 Cos[th1-#1]-r2 Cos[th2-#1]))-1.] < 1/10^10 &],2 \[Pi]]]],
+CompilationTarget->"C",RuntimeOptions->"Speed"];
+
 
 
 (* ::Text:: *)
-(*Returns the interval of \[Omega] over which \[Epsilon] is positive.*)
+(*Returns the interval of \[Omega] over which e is positive.*)
 
 
-\[Epsilon]IsPositiveInt[{r1_,\[Theta]1_},{r2_,\[Theta]2_}]:=Block[{asymp1,asymps,\[Epsilon]Is1},
-asymp1=Mod[ArcTan[-r1 Sin[\[Theta]1]+r2 Sin[\[Theta]2],r1 Cos[\[Theta]1]-r2 Cos[\[Theta]2]],\[Pi]];
-asymps={asymp1,asymp1+\[Pi]};
+eIsPositiveInt[{r1_, th1_}, {r2_, th2_}] := Block[{asymp1, asymps, eIs1}, 
+	asymp1 = Mod[ArcTan[-r1 Sin[th1]+r2 Sin[th2], r1 Cos[th1]-r2 Cos[th2]], \[Pi]];
+	asymps = {asymp1, asymp1+\[Pi]};
 
-\[Epsilon]Is1=\[Epsilon]Is1l[{r1,\[Theta]1},{r2,\[Theta]2}];
-If[\[Epsilon]Is1[[1]]>asymps[[1]]&&\[Epsilon]Is1[[2]]<asymps[[2]],
-Interval[{asymps[[1]],asymps[[2]]}],
-Interval[{asymps[[2]],2\[Pi]+asymps[[1]]}]
-]
+	eIs1 = eIs1l[{r1, th1}, {r2, th2}];
+	If[ eIs1[[1]] > asymps[[1]] && eIs1[[2]] < asymps[[2]],
+		Interval[{asymps[[1]],          asymps[[2]]}],
+		Interval[{asymps[[2]], 2\[Pi] + asymps[[1]]}]
+	]
 ];
+
 
 
 (* ::Text:: *)
 (*Returns the interval of \[Omega] over which p, the semi-latus rectum, is positive. If p is negative, we're on the wrong branch of a hyperbola.*)
 
 
-pIsPositiveInt[{r1_,\[Theta]1_},{r2_,\[Theta]2_}]:=Block[{asymptote,intersect,derivativeAtIntersectIsPositive,condition1,condition2},
-asymptote=Mod[ArcTan[-r1 Sin[\[Theta]1]+r2 Sin[\[Theta]2],r1 Cos[\[Theta]1]-r2 Cos[\[Theta]2]],\[Pi]];
-intersect=Mod[ArcTan[Sin[\[Theta]2]-Sin[\[Theta]1],Cos[\[Theta]1]-Cos[\[Theta]2]],\[Pi]];
-derivativeAtIntersectIsPositive=(r1-r2)  Sin[\[Theta]1-\[Theta]2]>=0;
-condition1=If[asymptote<intersect,
-If[derivativeAtIntersectIsPositive,
-Interval[{intersect,asymptote+\[Pi]}],
-Interval[{asymptote,intersect}]
-],
-If[derivativeAtIntersectIsPositive,
-Interval[{intersect,asymptote}],
-Interval[{asymptote,intersect+\[Pi]}]
-]
-];
-(*needed for \[Theta]1 and \[Theta]2 are \[Pi] apart*)
-If[asymptote==intersect,condition1=Interval[{asymptote,intersect+\[Pi]}]];
-{IntervalUnion[condition1-\[Pi],condition1,condition1+\[Pi]],intersect,asymptote}
+pIsPositiveInt[{r1_, th1_}, {r2_, th2_}] := Block[ {asymptote, intersect, derivativeAtIntersectIsPositive, condition1, condition2},
+	asymptote = Mod[ArcTan[-r1 Sin[th1]+r2 Sin[th2],r1 Cos[th1]-r2 Cos[th2]],\[Pi]];
+	intersect = Mod[ArcTan[Sin[th2]-Sin[th1],Cos[th1]-Cos[th2]],\[Pi]];
+	derivativeAtIntersectIsPositive = (r1-r2) Sin[th1-th2] >= 0;
+	condition1 = If[asymptote < intersect, 
+		If[derivativeAtIntersectIsPositive, 
+			Interval[{intersect, asymptote + \[Pi]}], 
+			Interval[{asymptote, intersect}]
+		], 
+		If[derivativeAtIntersectIsPositive,  
+			Interval[{intersect, asymptote}], 
+			Interval[{asymptote, intersect + \[Pi]}]
+		]
+	];
+	(*needed for th1 and th2 are \[Pi] apart*)
+	If[asymptote==intersect,  condition1 = Interval[{asymptote, intersect + \[Pi]}] ];
+
+	{IntervalUnion[condition1-\[Pi], condition1, condition1 + \[Pi]], intersect, asymptote}
 ];
 
 
 (* ::Text:: *)
 (*A helper function... given two 'points' between two 'asymptotes', it returns the set of intervals which are between the 'asymptotes' but outside the 'points'.*)
 
+
 createIntervalsFromPointsBetweenAsymptotes[p_,a_] := 
 If[ p[[2]] > a[[2]],
 	If[ p[[1]] > a[[2]],
-		Interval[{ a[[2]], p[[1]] }, { p[[2]], a[[1]]+2\[Pi] }],
+		Interval[{ a[[2]], p[[1]] }, { p[[2]], a[[1]] + 2\[Pi] }],
 		Interval[{ p[[1]], a[[1]] }, { a[[2]], p[[2]] }]],
 	If[ p[[1]] > a[[1]],
 		Interval[ {a[[1]] ,p[[1]] }, { p[[2]], a[[2]] }],
-		Interval[ {p[[2]] ,a[[1]] }, { a[[2]], p[[1]]+2\[Pi] }]
+		Interval[ {p[[2]] ,a[[1]] }, { a[[2]], p[[1]] + 2\[Pi] }]
 	]
 ];
 
 
-(* ::Text:: *)
-(*Returns the interval of \[Omega] over which \[Epsilon] is greater than 1, i.e. the orbit is hyperbolic.*)
-
-\[Epsilon]IsGt1Int[{r1_,\[Theta]1_},{r2_,\[Theta]2_}]:=Block[{asymp1,asymps,\[Epsilon]Is1,i},
-asymp1=Mod[ArcTan[-r1 Sin[\[Theta]1]+r2 Sin[\[Theta]2],r1 Cos[\[Theta]1]-r2 Cos[\[Theta]2]],\[Pi]];
-asymps={asymp1,asymp1+\[Pi]};
-
-\[Epsilon]Is1=\[Epsilon]Is1l[{r1,\[Theta]1},{r2,\[Theta]2}];
-i=createIntervalsFromPointsBetweenAsymptotes[\[Epsilon]Is1,asymps];
-IntervalUnion[i-2\[Pi],i,i+2\[Pi]]
-];
 
 (* ::Text:: *)
-(*Returns the interval of \[Omega] over which \[Epsilon] is less than than 1, i.e. the orbit is elliptical.*)
+(*Returns the interval of \[Omega] over which e is greater than 1, i.e. the orbit is hyperbolic.*)
 
-\[Epsilon]IsLt1Int[{r1_,\[Theta]1_},{r2_,\[Theta]2_}]:=Block[{asymp1,asymps,\[Epsilon]Is1,i},
-\[Epsilon]Is1=\[Epsilon]Is1l[{r1,\[Theta]1},{r2,\[Theta]2}];
-i=If[\[Epsilon]Is1[[2]]-\[Epsilon]Is1[[1]]<\[Pi],
-Interval[{\[Epsilon]Is1[[1]],\[Epsilon]Is1[[2]]}],
-Interval[{0,\[Epsilon]Is1[[1]]},{\[Epsilon]Is1[[2]],2\[Pi]}]
+
+eIsGt1Int[{r1_,th1_},{r2_,th2_}]:=Block[{asymp1,asymps,eIs1,i},
+	asymp1 = Mod[ArcTan[-r1 Sin[th1]+r2 Sin[th2],r1 Cos[th1]-r2 Cos[th2]],\[Pi]];
+	asymps = {asymp1,asymp1+\[Pi]};
+
+	eIs1 = eIs1l[{r1,th1},{r2,th2}];
+	i = createIntervalsFromPointsBetweenAsymptotes[eIs1,asymps];
+	IntervalUnion[i-2\[Pi],i,i+2\[Pi]]
 ];
-IntervalUnion[i-2\[Pi],i,i+2\[Pi]]
+
+
+
+(* ::Text:: *)
+(*Returns the interval of \[Omega] over which e is less than than 1, i.e. the orbit is elliptical.*)
+
+
+eIsLt1Int[{r1_, th1_}, {r2_, th2_}] := Block[{asymp1, asymps, eIs1, i}, 
+	eIs1 = eIs1l[{r1, th1}, {r2, th2}];
+	i = If[eIs1[[2]]-eIs1[[1]] < \[Pi],
+		Interval[{eIs1[[1]], eIs1[[2]]}],
+		Interval[{0, eIs1[[1]]}, {eIs1[[2]], 2\[Pi]}]
+	];
+	IntervalUnion[i-2\[Pi],i,i+2\[Pi]]
 ];
+
+
 
 (* ::Text:: *)
 (*Returns the interval over which (for a hyperbolic orbit with positive angular momentum h) the 'Point 2' is reached after 'Point 1'*)
 
-pt2FollowsPt1ForPoshInt[\[Theta]1_,\[Theta]2_]:=Module[{landing1,landing2,\[Theta]1m,\[Theta]2m,i},
-\[Theta]1m=Mod[\[Theta]1,2\[Pi]];
-\[Theta]2m=Mod[\[Theta]2,2\[Pi]];
-landing1=Mod[\[Theta]1m+\[Pi],2\[Pi]];
-landing2=Mod[\[Theta]2m+\[Pi],2\[Pi]];
-i=If[landing1<landing2,
-Interval[{0,landing1},{landing2,2\[Pi]}],
-Interval[{landing2,landing1}]
+
+pt2FollowsPt1ForPoshInt[th1_, th2_]:=Module[{landing1, landing2, th1m, th2m, i}, 
+	th1m = Mod[th1, 2\[Pi]];
+	th2m = Mod[th2, 2\[Pi]];
+	landing1 = Mod[th1m+\[Pi], 2\[Pi]];
+	landing2 = Mod[th2m+\[Pi], 2\[Pi]];
+	i = If[landing1<landing2, 
+		Interval[{0, landing1}, {landing2, 2\[Pi]}], 
+		Interval[{landing2, landing1}]
+	];
+	IntervalUnion[i, i+2\[Pi]]
 ];
-IntervalUnion[i,i+2\[Pi]]
-];
+
+
 
 (* ::Text:: *)
 (*Same thing but for negative angular momentum hyperbolic orbits*)
 
-pt2FollowsPt1ForNeghInt[\[Theta]1_,\[Theta]2_]:=Module[{i},
-i=If[\[Theta]1<\[Theta]2,
-Interval[{\[Theta]1,\[Theta]2}],
-Interval[{0,\[Theta]2},{\[Theta]1,2\[Pi]}]
-];
-IntervalUnion[i-\[Pi],i+\[Pi],i+3\[Pi]]
+
+pt2FollowsPt1ForNeghInt[th1_, th2_]:=Module[{i}, 
+	i = If[th1 < th2,
+		Interval[{th1,th2}],
+		Interval[{0, th2}, {th1, 2\[Pi]}]
+	];
+	IntervalUnion[i - \[Pi], i + \[Pi], i + 3\[Pi]]
 ];
 
 (* ::Section:: *)
 (*Combine the various contraints*)
 
-(* ::Text:: *)
-(*\[Omega] is acceptable if p is positive (orbit is not on the wrong branch of a hyperbola) and if \[Epsilon]>1 (it's a hyperbola) then it has to reach 'point 2' after 'point 1'.*)
+(*\[Omega] is acceptable if p is positive (orbit is not on the wrong branch of a hyperbola) and if e>1 (it's a hyperbola) then it has to reach 'point 2' after 'point 1'.*)
+Acceptable\[Omega]Interval[{r1_, th1_}, {r2_, th2_}] := Block[{ppos, epos, egt1, elt1, hPosOk, hNegOk}, 
+	ppos = pIsPositiveInt[{r1, th1}, {r2, th2}];
+	egt1 = eIsGt1Int[{r1, th1}, {r2, th2}];
+	elt1 = eIsLt1Int[{r1, th1}, {r2, th2}];
+	hPosOk = pt2FollowsPt1ForPoshInt[th1, th2];
+	hNegOk = pt2FollowsPt1ForNeghInt[th1, th2];
 
-(*The more concise version*)
-\[Omega]AcceptableInts[{r1_,\[Theta]1_},{r2_,\[Theta]2_}]:=Block[{ppos,\[Epsilon]pos,\[Epsilon]gt1,\[Epsilon]lt1,hPosOk,hNegOk},
-ppos=pIsPositiveInt[{r1,\[Theta]1},{r2,\[Theta]2}];
-\[Epsilon]gt1=\[Epsilon]IsGt1Int[{r1,\[Theta]1},{r2,\[Theta]2}];
-\[Epsilon]lt1=\[Epsilon]IsLt1Int[{r1,\[Theta]1},{r2,\[Theta]2}];
-hPosOk=pt2FollowsPt1ForPoshInt[\[Theta]1,\[Theta]2];
-hNegOk=pt2FollowsPt1ForNeghInt[\[Theta]1,\[Theta]2];
-
-{{1,IntervalIntersection[ppos,IntervalUnion[\[Epsilon]lt1,IntervalIntersection[\[Epsilon]gt1,hPosOk]]]},{-1,IntervalIntersection[ppos,IntervalUnion[\[Epsilon]lt1,IntervalIntersection[\[Epsilon]gt1,hNegOk]]]}}
+	{{1, IntervalIntersection[ppos, IntervalUnion[elt1, IntervalIntersection[egt1, hPosOk]]]}, {-1, IntervalIntersection[ppos, IntervalUnion[elt1, IntervalIntersection[egt1, hNegOk]]]}}
 ]
+
 (*The same thing but with some de-duplication of effort... might be 50us faster.*)
-\[Omega]AcceptableInts[{r1_,\[Theta]1_},{r2_,\[Theta]2_}]:=Block[{asymp1,asymps,intersect,goesUpAtIntersect,condition1,\[Epsilon]Is1,ppos,i,\[Epsilon]pos,\[Epsilon]gt1,\[Epsilon]lt1,hPosOk,hNegOk},
-asymp1=Mod[ArcTan[-r1 Sin[\[Theta]1]+r2 Sin[\[Theta]2],r1 Cos[\[Theta]1]-r2 Cos[\[Theta]2]],\[Pi]];
-asymps={asymp1,asymp1+\[Pi]};
+Acceptable\[Omega]Interval[{r1_, th1_}, {r2_, th2_}] := Block[ {asymp1, asymps, intersect, goesUpAtIntersect, condition1, eIs1, ppos, i, epos, egt1, elt1, hPosOk, hNegOk}, 
+	asymp1 = Mod[ArcTan[-r1 Sin[th1]+r2 Sin[th2], r1 Cos[th1]-r2 Cos[th2]], \[Pi]];
+	asymps = {asymp1, asymp1+\[Pi]};
 
-\[Epsilon]Is1=\[Epsilon]Is1l[{r1,\[Theta]1},{r2,\[Theta]2}];
+	eIs1 = eIs1l[{r1, th1}, {r2, th2}];
 
-(*get where P is ok*)
-intersect=Mod[ArcTan[Sin[\[Theta]2]-Sin[\[Theta]1],Cos[\[Theta]1]-Cos[\[Theta]2]],\[Pi]];
-goesUpAtIntersect=(r1-r2)  Sin[\[Theta]1-\[Theta]2]>0;
-condition1=If[asymps[[1]]<intersect,
-If[goesUpAtIntersect,
-Interval[{intersect,asymps[[2]]}],
-Interval[{asymps[[1]],intersect}]
-],
-If[goesUpAtIntersect,
-Interval[{intersect,asymps[[1]]}],
-Interval[{asymps[[1]],intersect+\[Pi]}]
+	(*get where P is ok*)
+	intersect = Mod[ArcTan[Sin[th2]-Sin[th1], Cos[th1]-Cos[th2]], \[Pi]];
+	goesUpAtIntersect = (r1-r2)  Sin[th1-th2]>0;
+	condition1 = If[ asymps[[1]] < intersect, 
+		If[ goesUpAtIntersect, 
+			Interval[{intersect, asymps[[2]]}], 
+			Interval[{asymps[[1]], intersect}]
+		], 
+		If[ goesUpAtIntersect, 
+			Interval[{intersect, asymps[[1]]}], 
+			Interval[{asymps[[1]], intersect+\[Pi]}]
+		]
+	];
+	If[ asymps[[1]]==intersect, condition1 = Interval[{asymps[[1]], asymps[[1]]+\[Pi]}]];
+	ppos = IntervalUnion[condition1-\[Pi], condition1, condition1+\[Pi]];
+
+	(*get where e is gt 1*)
+	i = createIntervalsFromPointsBetweenAsymptotes[eIs1, asymps];
+	egt1 = IntervalUnion[i-2\[Pi], i, i+2\[Pi]];
+
+	(*get where e is lt 1*)
+	i = If[ eIs1[[2]]-eIs1[[1]] < \[Pi], 
+		Interval[{eIs1[[1]], eIs1[[2]]}], 
+		Interval[{0, eIs1[[1]]}, {eIs1[[2]], 2\[Pi]}]
+	];
+	elt1 = IntervalUnion[i-2\[Pi], i, i+2\[Pi]];
+
+	hPosOk = pt2FollowsPt1ForPoshInt[th1, th2];
+	hNegOk = pt2FollowsPt1ForNeghInt[th1, th2];
+
+	{{1, IntervalIntersection[ppos, IntervalUnion[elt1, IntervalIntersection[egt1, hPosOk]]]}, {-1, IntervalIntersection[ppos, IntervalUnion[elt1, IntervalIntersection[egt1, hNegOk]]]}}
 ]
-];
-If[asymps[[1]]==intersect,condition1=Interval[{asymps[[1]],asymps[[1]]+\[Pi]}]];
-ppos=IntervalUnion[condition1-\[Pi],condition1,condition1+\[Pi]];
 
-(*get where \[Epsilon] is gt 1*)
-i=createIntervalsFromPointsBetweenAsymptotes[\[Epsilon]Is1,asymps];
-\[Epsilon]gt1=IntervalUnion[i-2\[Pi],i,i+2\[Pi]];
+End[];
 
-(*get where \[Epsilon] is lt 1*)
-i=If[\[Epsilon]Is1[[2]]-\[Epsilon]Is1[[1]]<\[Pi],
-Interval[{\[Epsilon]Is1[[1]],\[Epsilon]Is1[[2]]}],
-Interval[{0,\[Epsilon]Is1[[1]]},{\[Epsilon]Is1[[2]],2\[Pi]}]
-];
-\[Epsilon]lt1=IntervalUnion[i-2\[Pi],i,i+2\[Pi]];
+(*SetAttributes[#, {Protected,ReadProtected}]& /@ Names["`*"];*)
 
-hPosOk=pt2FollowsPt1ForPoshInt[\[Theta]1,\[Theta]2];
-hNegOk=pt2FollowsPt1ForNeghInt[\[Theta]1,\[Theta]2];
-
-{{1,IntervalIntersection[ppos,IntervalUnion[\[Epsilon]lt1,IntervalIntersection[\[Epsilon]gt1,hPosOk]]]},{-1,IntervalIntersection[ppos,IntervalUnion[\[Epsilon]lt1,IntervalIntersection[\[Epsilon]gt1,hNegOk]]]}}
-]
-
-
-(* ::Input:: *)
-(*Manipulate[{Timing[\[Omega]AcceptableInts[{r1,\[Theta]1},{r2,\[Theta]2}]],Show[OrbitPlot[p[r1,\[Theta]1,r2,\[Theta]2,\[Omega]],\[Epsilon][r1,\[Theta]1,r2,\[Theta]2,\[Omega]],\[Omega]],ListPolarPlot[{{{\[Theta]1,r1}},{{\[Theta]2,r2}}},PlotStyle->{Green,Red}]]},{{r1,1},0.1,4},{\[Theta]1,.1,2\[Pi]},{{r2,4},0.1,4},{{\[Theta]2,1.},0,2\[Pi]},{{vr1,0},-4,4},{{v\[Theta]1,1},-4,4},{{vr2,0},-4,4},{{v\[Theta]2,.3},-4,4},{\[Omega],0,2\[Pi]}]*)
-
-
-
+EndPackage[];
