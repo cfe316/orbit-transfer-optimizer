89dc1e646dfde87f971b68d26529b7b454c0d6a4
Jacob Schwartz <thesquarerootofjacob@gmail.com>
2015-02-14 14:15:52 -0500

    Plots are prettier now.

--- OrbitTransferOptimizer/Plots.m
+++ OrbitTransferOptimizer/Plots.m
@@ -23,18 +23,37 @@ Begin["Private`"];
 
 OrbitPlot3D[orb_Association] := OrbitPlot3D[{orb}];
 
-OrbitPlot3D[orbs_List] := Block[{},
-	Graphics3D[OrbitLine3D[#]& /@ orbs, PlotRange->{{-5,5},{-5,5},{-5,5}}]
+OrbitPlot3D[orbs_?(Length[#]!=3 &)] := Block[{},
+	Graphics3D[{Point[{0,0,0}],
+		OrbitLine3D[#]& /@ orbs}
+	]
+]
+
+OrbitPlot3D[orbs_?(Length[#]==3 &)] := Block[{},
+	Graphics3D[{
+		Point[{0,0,0}],
+		Arrowheads[Small],
+		Red, OrbitLine3D[orbs[[1]]], OrbitVelocityArrow3D[orbs[[1]]],
+		Green, OrbitLine3D[orbs[[2]]], OrbitVelocityArrow3D[orbs[[2]]],
+		Black, OrbitLine3D[orbs[[3]]], OrbitVelocityArrow3D[orbs[[3]]]}
+	]
 ]
 
 OrbitLine3D[o_?(AssociationQ[#] && KeyExistsQ[#, "Orbit"] && #["Orbit"] == "Nondegenerate" &)] := Block[{Nurl, Nurh, Nus},
 	{Nurl, Nurh} = o["\[Nu]Range"];
-	Nus = Max[Abs[(Nurh - Nurl)/40], 0.05];
+	Nus = Max[Abs[(Nurh - Nurl)/80], 0.05];
 	Line[
 		Table[
 			CartesianFromKeplerian[CoordinateFromOrbit[o, Nu]]["Position"]
 		, {Nu, Nurl, Nurh, Nus}]
 	]
+]
+
+OrbitVelocityArrow3D[o_?(AssociationQ[#] && KeyExistsQ[#, "Orbit"] && #["Orbit"] == "Nondegenerate" &)] := Block[{cart, p, v},
+	cart= CartesianFromKeplerian[CoordinateFromOrbit[o,0]];
+	p = cart["Position"];
+	v = Normalize[cart["Velocity"]] Max[Norm[p], 1];
+	Arrow[Line[{p, p +v}]]
 ]
 
 End[];
