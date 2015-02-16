1c18164a0be53f90068609e5d162f477eae09a88
Jacob Schwartz <thesquarerootofjacob@gmail.com>
2015-02-16 01:49:18 -0500

    Added nu calculation to ProcessBurn

--- OrbitTransferOptimizer/BestTransferTwoOrbits.m
+++ OrbitTransferOptimizer/BestTransferTwoOrbits.m
@@ -3,17 +3,22 @@
 (* ::Section:: *)
 (* Keplerian to Cartesian Elements package: Title and comments *)
 
+
 (* :Title: OrbitTransferOptimizer *)
 (* :Context: OrbitTransferOptimizer`BestTransferTwoOrbits *)
 (* :Author: Jacob Schwartz (thesquarerootofjacob@gmail.com) *)
 
+
+
 (* ::Section:: *)
 (* Begin package and help *)
+
 
 BeginPackage["OrbitTransferOptimizer`BestTransferTwoOrbits`",
 				{"OrbitTransferOptimizer`BestTransferTwoPoints`",
 				 "OrbitTransferOptimizer`OrbitCoordinateTransformation`",
-				 "OrbitTransferOptimizer`Utilities`"
+				 "OrbitTransferOptimizer`Utilities`",
+				 "OrbitTransferOptimizer`Heading`"
 				}];
 
 Unprotect@"`*";
@@ -25,18 +30,16 @@ ProcessBurn::usage = "Compute ancillary information from Burn 1 and Burn 2";
 
 Begin["Private`"];
 
-End[];
-
 BestTransferTwoOrbits[oo1_, oo2_]:= Block[{
  t, o1, o2, nul1, nuh1, nul2, nuh2, 
  minnustep, nustep1, nustep2, mint},
  
 	{o1, o2} = restrictOrbit[#]& /@ {oo1, oo2};
-	minnustep = \[Pi]/8;
+	minnustep = \[Pi]/12.-0.00001;
 	{nul1, nuh1} = o1["\[Nu]Range"];
-	nustep1 = Min[(nuh1 - nul1)/8., minnustep];
+	nustep1 = Min[(nuh1 - nul1)/12., minnustep];
 	{nul2, nuh2} = o2["\[Nu]Range"];
-	nustep2 = Min[(nuh2 - nul2)/8., minnustep];
+	nustep2 = Min[(nuh2 - nul2)/12., minnustep];
 
 	(* Use brute-force search *)
 	t = Flatten[
@@ -53,11 +56,15 @@ BestTransferTwoOrbits[oo1_, oo2_]:= Block[{
 ProcessBurn[b_] := Module[{ob},
 	ob = b;
 	ob["Radius"] = Norm[b["Position"]];
+	ob["Speed"] = Norm[b["Velocity"]];
 	ob["\[CapitalDelta]V"] = Norm[b["VelocityChange"]];
-	ob["KSP Navball"] = OrbitTransferOptimizer`Heading`KSPHeadingFromCartesian[ob]["KSPNavball"];
-	ob["LVLH Heading"] = OrbitTransferOptimizer`Heading`LVLHHeadingFromCartesian[ob]["LVLH"];
+	ob["KSP Navball"] = KSPHeadingFromCartesian[ob]["KSPNavball"];
+	ob["LVLH Heading"] = LVLHHeadingFromCartesian[ob]["LVLH"];
+	ob["\[Nu]"] = KeplerianFromCartesian[ob]["\[Nu]"];
 	ob
 ]
+
+End[];
 
 (*SetAttributes[#, {Protected,ReadProtected}]& /@ Names["`*"];*)
 
